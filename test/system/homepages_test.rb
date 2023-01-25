20da547b207c361ce803d4c20b59ea616c5bd23e
Niklas Haeusele <niklas.haeusele@hey.com>
2023-01-25 21:52:51 +0100

    bundle exec standardrb --fix

--- test/system/homepages_test.rb
+++ test/system/homepages_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class HomepagesTest < ApplicationSystemTestCase
   test "visit root" do
     visit root_url
-  
+
     assert_selector "h1", text: "Hello World"
   end
 end
